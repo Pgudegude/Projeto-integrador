18ca09603eac07b4e5fcc81ad986ddd7889e7936
Treinamento <Treinamento@RD723935.raiadrogasil.com>
2020-02-28 14:23:49 -0300

    atualizando o login

--- ProjetoFinalCurso/js/script.js
+++ ProjetoFinalCurso/js/script.js
@@ -4,3 +4,18 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+
+
+var password = document.getElementById("password")
+  , confirm_password = document.getElementById("confirm_password");
+  
+  function validatePassword(){
+    if(password.value != confirm_password.value) {
+      confirm_password.setCustomValidity("Senhas diferentes!");
+    } else {
+      confirm_password.setCustomValidity('');
+    }
+  }
+  
+  password.onchange = validatePassword;
+  confirm_password.onkeyup = validatePassword;
