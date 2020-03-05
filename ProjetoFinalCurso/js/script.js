3cc86e6624eaa266102fc340f24852a5b27545b5
Treinamento <Treinamento@RD723935.raiadrogasil.com>
2020-03-05 09:28:23 -0300

    atualização 01

--- ProjetoFinalCurso/js/script.js
+++ ProjetoFinalCurso/js/script.js
@@ -1,34 +1,29 @@
 var modal = document.getElementById('id01');
 var overlay = document.getElementById('overlay');
-// window.onclick = function(event) {
-//     if (event.target == modal) {
-//         modal.style.display = "none";
-//     }
-// }
 
 abrirPopup = () => {
-  modal.style.display = "block";
-  overlay.style.display = "block";
+    modal.style.display = "block";
+    overlay.style.display = "block";
 }
 
 fecharPopup = () => {
-  modal.style.display = "none";
-  overlay.style.display = "none";
+    modal.style.display = "none";
+    overlay.style.display = "none";
 }
 
-let esqueceu = document.getElementById('btnSenha');
-
-
 var password = document.getElementById("password")
-  , confirm_password = document.getElementById("confirm_password");
-  
-  function validatePassword(){
-    if(password.value != confirm_password.value) {
-      confirm_password.setCustomValidity("Senhas diferentes!");
+var confirm_password = document.getElementById("confirm_password");
+
+function validatePassword() {
+    if (password.value != confirm_password.value) {
+        confirm_password.setCustomValidity("Senhas diferentes!");
+
     } else {
-      confirm_password.setCustomValidity('');
+        confirm_password.setCustomValidity('');
+
+
     }
-  }
-  
-  password.onchange = validatePassword;
-  confirm_password.onkeyup = validatePassword;
+}
+
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
