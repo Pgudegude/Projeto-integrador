7a8023c9dd494d21fa52a7faadea916c35c8b3b4
Treinamento <Treinamento@RD723910.raiadrogasil.com>
2020-03-06 15:20:57 -0300

    finalizado

--- JS/main.js
+++ JS/main.js
@@ -1,35 +1,18 @@
-let incluirCarrinho = document.getElementById("btnIncluir");
-let carrinho = document.getElementById('carrinho');
+let cartItems = document.getElementById('count-items');
+let cont = 0;
 
-incluirCarrinho.addEventListener('click', () => {
-    let elemento = carrinho.children[0].children[1].textContent;
+const addToCart = () => {
+    cont++;
+    cartItems.innerHTML = cont;
+}
 
-    let recebe = `<span id="contador" class="badge badge-dark rounded-circle">${elemento++}</span>`
-
-
-
-})
-
-
-
-
-// function TestaCPF(strCPF) {
-//     let Soma;
-//     let Resto;
-//     Soma = 0;
-//     if (strCPF == "00000000000") return false;
-
-//     for (i = 1; i <= 9; i++) Soma = Soma + parseInt(strCPF.substring(i - 1, i)) * (11 - i);
-//     Resto = (Soma * 10) % 11;
-
-//     if ((Resto == 10) || (Resto == 11)) Resto = 0;
-//     if (Resto != parseInt(strCPF.substring(9, 10))) return false;
-
-//     Soma = 0;
-//     for (i = 1; i <= 10; i++) Soma = Soma + parseInt(strCPF.substring(i - 1, i)) * (12 - i);
-//     Resto = (Soma * 10) % 11;
-
-//     if ((Resto == 10) || (Resto == 11)) Resto = 0;
-//     if (Resto != parseInt(strCPF.substring(10, 11))) return false;
-//     return true;
-// }
+{/* <div class="card" style="width: 18rem;">
+  <div class="card-header">
+    Featured
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">Cras justo odio</li>
+    <li class="list-group-item">Dapibus ac facilisis in</li>
+    <li class="list-group-item">Vestibulum at eros</li>
+  </ul>
+</div> */}
