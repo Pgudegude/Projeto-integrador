676f5fa43d16da1e8914f3d6fdaa609ea0c917aa
Treinamento <Treinamento@RD723925.raiadrogasil.com>
2020-03-06 15:31:29 -0300

    melhorando header e footer

--- Projeto-integrador/src/app/app.module.ts
+++ Projeto-integrador/src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { ContatoComponent } from './components/contato/contato.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ContatoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Projeto-integrador/src/app/components/header/header.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+
+  // cont: number = 0;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  // countItems(){
+  //   this.cont ++;
+    
+  // }
+
+}
