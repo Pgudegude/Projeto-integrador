758a257816054d1bb67a9db4df345b8a096c6dfd
Treinamento <Treinamento@RD723925.raiadrogasil.com>
2020-03-13 08:27:47 -0300

    sem carousel

--- Projeto-integrador/src/app/components/carrossel/carrossel.component.ts
+++ Projeto-integrador/src/app/components/carrossel/carrossel.component.ts
@@ -1,16 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 
 
 @Component({
   selector: 'app-carrossel',
   templateUrl: './carrossel.component.html',
   styleUrls: ['./carrossel.component.css'],
-  providers: [NgbCarouselConfig]
 })
 export class CarrosselComponent implements OnInit {
-  images = ["bolasDeLeite", "bolasColoridas", "bolasEspelhadas"].map((n) => `../../../assets/Imagem/${n}.jpg`);
-  constructor() { }
 
   ngOnInit(): void {
   }
